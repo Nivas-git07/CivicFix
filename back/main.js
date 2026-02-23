dbe91a1e3c4ca0fc613c35b71e3d58b17ca8d532
Nivas-git07 <itsmenivas007@gmail.com>
2026-02-23 08:48:17 +0000

    change into production

--- back/main.js
+++ back/main.js
@@ -36,7 +36,7 @@ pool.connect()
 
 
 app.use(cors({
-  origin: ["https://civicfix.selfmade.solutions"],
+  origin: ["https://civicfix-nps2.onrender.com"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
